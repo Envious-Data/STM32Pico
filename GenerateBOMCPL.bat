9b60cd9b06cb591d2bf4174ab57b4f27e869be52
EnviousData <Enviousdata@gmail.com>
2021-07-31 09:45:28 +0100

    Generated some JLCPCB compatible Gerbers.

--- GenerateBOMCPL.bat
+++ GenerateBOMCPL.bat
@@ -1,2 +1,5 @@
-jlc-kicad-tools -n EnvOpenPico . -o .\_output\ --include-all-component-groups
+@echo off
+rem for what ever reason windows 10 has decided to be anoying.
+rem ok so i fixed ^ my issues, for some reason python stuff isnt exported to path in environment variables in windows...
+jlc-kicad-tools -n STM32Pico . -o _output --include-all-component-groups
 pause
